9121b96a8449c831e707ba4a2c5758c8be0d10fd
Guilherme Lawisch <lawisch03@gmail.com>
2021-04-17 14:36:50 -0300

    Add ts configuration and paths

---
+++ src/controllers/JobController.ts
@@ -0,0 +1,7 @@
+import { Job } from "@models/Job";
+
+export class JobController {
+  show(req, res) {
+    const user = new Job();
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import '@controllers/JobController';
 
 const app = express();
 
