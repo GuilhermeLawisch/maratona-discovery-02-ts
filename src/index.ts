e275d4b3f76349d40e2c7b41f717b33aea7e8ba1
Guilherme Lawisch <lawisch03@gmail.com>
2021-04-17 14:07:41 -0300

    Add Typescript and ts-nod-dev

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  return res.json({ massage: 'Hello World' })
+})
+
+app.listen(3333, () => {
+  console.log('Server is running in http://localhost:3333')
+})
+
